f1f7dbcc3a19deb57f79358e8622d1c0a1f7596e
Lam Nguyen <lamngx@protonmail.ch>
2024-10-10 16:35:12 +0700

    postgresql test

--- PawFund.Infrastructure/Startup.cs
+++ PawFund.Infrastructure/Startup.cs
@@ -19,7 +19,7 @@ namespace PawFund
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddRazorPages();
             services.AddHttpClient();
