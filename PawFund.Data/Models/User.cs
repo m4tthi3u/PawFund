0260a077b2af63cc55ad836aded3ee24aee04c28
Lâm Nguyễn <takanashirikk4@proton.me>
2024-11-05 18:05:52 +0700

    no need for Password anymore

--- PawFund.Data/Models/User.cs
+++ PawFund.Data/Models/User.cs
@@ -6,7 +6,6 @@ namespace PawFund.Data.Models
         public int Id { get; set; }
         public string Username { get; set; }
         public string Email { get; set; }
-        //public string Password { get; set; }
         public string PasswordHash { get; set; }
         public UserRole Role { get; set; }
     }
