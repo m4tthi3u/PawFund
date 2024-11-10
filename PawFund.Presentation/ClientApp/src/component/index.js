b791919f972ee982db77c994f04cc936f409c1a2
hngle <lehuyhoang840@gmail.com>
2024-11-10 15:30:47 +0700

    remove donationcard

--- PawFund.Presentation/ClientApp/src/component/index.js
+++ PawFund.Presentation/ClientApp/src/component/index.js
@@ -1,6 +1,5 @@
 import {default as AdoptCard} from "./AdoptCard";
-import {default as DonationCard} from "./DonationCard";
 import {default as Quantity} from "./Quantity";
 
 
-export {AdoptCard, Quantity, DonationCard};
+export {AdoptCard, Quantity};
