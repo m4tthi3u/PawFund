8cf8b6560d8da3d9d9499e3203d58c07e7f64b03
hngle <lehuyhoang840@gmail.com>
2024-11-10 15:31:36 +0700

    small changes

--- PawFund.Presentation/ClientApp/src/index.js
+++ PawFund.Presentation/ClientApp/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import RouterCustom from './router';
-import './styles/styles.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import RouterCustom from "./router";
+import "./styles/styles.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
-        <RouterCustom />
-    </BrowserRouter>
+  <BrowserRouter>
+    <RouterCustom />
+  </BrowserRouter>
 );
