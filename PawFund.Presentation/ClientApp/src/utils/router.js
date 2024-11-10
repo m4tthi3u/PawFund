985941a2bdba64e6c2f3b8dff550840cb8eebc08
hngle <lehuyhoang840@gmail.com>
2024-11-10 15:35:47 +0700

    Update router.js

--- PawFund.Presentation/ClientApp/src/utils/router.js
+++ PawFund.Presentation/ClientApp/src/utils/router.js
@@ -1,21 +1,18 @@
 export const ROUTER = {
-
-    //đường dẫn đến trang
-    USER: {
-        HOME: "",
-        ADOPT: "/nhan-nuoi",
-        ADOPT_DOG: "/nhan-nuoi/cho",
-        ADOPT_CAT: "/nhan-nuoi/meo",
-        ADOPT_ORTHER: "/nhan-nuoi/khac",
-        ADOPTS: "/nhan-nuoi/chi-tiet/:id",
-        HOME_CART: "/nha",
-        DONATE: "/ung-ho",
-        VOLUNTEER: "/tinh-nguyen-vien",
-        CONTACT: "/lien-he",
-        PROFILE: "thong-tin-ca-nhan",
-
-
-
-
-    },
+  //đường dẫn đến trang
+  USER: {
+    HOME: "/",
+    ADOPT: "/nhan-nuoi",
+    ADOPT_DOG: "/nhan-nuoi/cho",
+    ADOPT_CAT: "/nhan-nuoi/meo",
+    ADOPT_ORTHER: "/nhan-nuoi/khac",
+    ADOPTS: "/nhan-nuoi/chi-tiet/:id",
+    HOME_CART: "/nha",
+    DONATE: "/ung-ho",
+    VOLUNTEER: "/tinh-nguyen-vien",
+    CONTACT: "/lien-he",
+    PROFILE: "/thong-tin-ca-nhan",
+    LOGIN: "/dang-nhap",
+    REGISTER: "/dang-ky",
+  },
 };
