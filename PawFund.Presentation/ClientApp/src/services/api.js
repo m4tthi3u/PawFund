128e32ef44a87b4cd9663df727bc62f90e7194cd
hngle <lehuyhoang840@gmail.com>
2024-11-10 15:35:01 +0700

    Update api.js

--- PawFund.Presentation/ClientApp/src/services/api.js
+++ PawFund.Presentation/ClientApp/src/services/api.js
@@ -1,6 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'https://localhost:7112/api'
+  baseURL: process.env.REACT_APP_API_URL || "https://localhost:7112/api",
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default api;
