c45f7488959e70c87cc4003ebd0603bb5ca4330a
hngle <lehuyhoang840@gmail.com>
2024-11-10 15:34:58 +0700

    Update router.js

--- PawFund.Presentation/ClientApp/src/router.js
+++ PawFund.Presentation/ClientApp/src/router.js
@@ -10,57 +10,65 @@ import HomeCartPage from "pages/users/homeCartPage";
 import DonationPage from "pages/users/donationPage";
 import VolunteerPage from "pages/users/volunteerPage";
 import ContactPage from "pages/users/contactPage";
+import Login from "pages/users/auth/Login";
+import Register from "pages/users/auth/Register";
 
 const renderUserRouter = () => {
-    const userRouters = [
-        {
-            path: ROUTER.USER.HOME,
-            Component: <HomePage />
-        },
-        {
-            path: ROUTER.USER.PROFILE,
-            Component: <ProfilePage />
-        },
-        {
-            path: ROUTER.USER.ADOPT,
-            Component: <AdoptPage />,
-        },
-        {
-            path: ROUTER.USER.ADOPTS,
-            Component: <AdoptDetailPage />,
-        },
-        {
-            path: ROUTER.USER.HOME_CART,
-            Component: <HomeCartPage />,
-        },
-        {
-            path: ROUTER.USER.DONATE,
-            Component: <DonationPage />,
-        },
-        {
-            path: ROUTER.USER.VOLUNTEER,
-            Component: <VolunteerPage />,
-        },
-        {
-            path: ROUTER.USER.CONTACT,
-            Component: <ContactPage />,
-        },
-        
-    ];
+  const userRouters = [
+    {
+      path: ROUTER.USER.HOME,
+      Component: <HomePage />,
+    },
+    {
+      path: ROUTER.USER.PROFILE,
+      Component: <ProfilePage />,
+    },
+    {
+      path: ROUTER.USER.ADOPT,
+      Component: <AdoptPage />,
+    },
+    {
+      path: ROUTER.USER.ADOPTS,
+      Component: <AdoptDetailPage />,
+    },
+    {
+      path: ROUTER.USER.HOME_CART,
+      Component: <HomeCartPage />,
+    },
+    {
+      path: ROUTER.USER.DONATE,
+      Component: <DonationPage />,
+    },
+    {
+      path: ROUTER.USER.VOLUNTEER,
+      Component: <VolunteerPage />,
+    },
+    {
+      path: ROUTER.USER.CONTACT,
+      Component: <ContactPage />,
+    },
+  ];
 
-    return (
-        <MasterLayout>
-            <Routes>
-                {userRouters.map((item, key) => (
-                    <Route key={key} path={item.path} element={item.Component} />
-                ))}
-            </Routes>
-        </MasterLayout>
-    );
+  return (
+    // <MasterLayout>
+    <Routes>
+      <Route element={<MasterLayout />}>
+        {userRouters.map((item, key) => (
+          <Route key={key} path={item.path} element={item.Component} />
+        ))}
+      </Route>
+      <Route path={ROUTER.USER.LOGIN} element={<Login />} />
+      <Route path={ROUTER.USER.REGISTER} element={<Register />} />
+      {/* {userRouters.map((item, key) => (
+          <Route key={key} path={item.path} element={item.Component} />
+        ))} */}
+    </Routes>
+    // </MasterLayout>
+  );
 };
 
 const RouterCustom = () => {
-    return renderUserRouter();
+  return renderUserRouter();
 };
 
 export default RouterCustom;
