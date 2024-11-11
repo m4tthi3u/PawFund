89ced831c87ce4338f82570141c96da3ec32553b
Lâm Nguyễn <takanashirikk4@proton.me>
2024-11-11 09:57:14 +0700

    Clean ups.

--- PawFund.Presentation/Startup.cs
+++ PawFund.Presentation/Startup.cs
@@ -1,4 +1,3 @@
-using System.Security.Claims;
 using System.Text;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -11,12 +10,11 @@ using PawFund.Data.Context;
 using PawFund.Data.Repositories.Implementations;
 using PawFund.Data.Repositories.Interfaces;
 using PawFund.Presentation.Hubs;
-using PawFund.Web.Server.Repositories;
-using PawFund.Web.Server.Services;
 
-namespace PawFund.Presentation;
+namespace PawFund.Presentation
+{
 
-public class Startup
+    public class Startup
     {
         public IConfiguration Configuration { get; }
 
@@ -27,7 +25,8 @@ public class Startup
 
         public void ConfigureServices(IServiceCollection services)
         {
-            var key = Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]) ?? throw new InvalidOperationException("JWT Key not found");
+            var key = Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]) ??
+                      throw new InvalidOperationException("JWT Key not found");
             services.AddAuthentication(option =>
                 {
                     option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -57,36 +56,37 @@ public class Startup
                 options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddLogging();
             services.AddSignalR();
-            
+
             services.AddScoped<IPetRepository, PetRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IShelterRepository, ShelterRepository>();
             services.AddScoped<IDonationRepository, DonationRepository>();
             services.AddScoped<IEventRepository, EventRepository>();
             services.AddScoped<IUserPetRepository, UserPetRepository>();
-            
+
             services.AddScoped<IPetService, PetService>();
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<IShelterService, ShelterService>();
             services.AddScoped<IDonationService, DonationService>();
             services.AddScoped<IEventService, EventService>();
             services.AddScoped<IUserPetService, UserPetService>();
-            
+
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PawFund API", Version = "v1" });
                 c.UseInlineDefinitionsForEnums();
-                
+
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
-                    Description = "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below.",
+                    Description =
+                        "JWT Authorization header using the Bearer scheme. Enter 'Bearer' [space] and then your token in the text input below.",
                     Name = "Authorization",
                     In = ParameterLocation.Header,
                     Type = SecuritySchemeType.ApiKey,
                     Scheme = "Bearer"
                 });
-                
+
                 c.AddSecurityRequirement(new OpenApiSecurityRequirement()
                 {
                     {
@@ -104,14 +104,14 @@ public class Startup
             });
 
             services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy",
-        builder => builder
-            .WithOrigins("http://localhost:3000", "http://localhost:3001")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials());
-});
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                        .WithOrigins("http://localhost:3000", "http://localhost:3001")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials());
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -120,10 +120,7 @@ public class Startup
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PawFund API v1");
-                });
+                app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "PawFund API v1"); });
             }
 
             app.UseHttpsRedirection();
@@ -138,3 +135,4 @@ public class Startup
             });
         }
     }
+}
